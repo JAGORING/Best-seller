608ae3f7779a80de77bfe24dbc80803b695e2b34
JAGORING <lovehaha11@gmail.com>
2024-11-16 03:08:39 +0900

    [Feat] loading 적용

---
+++ src/app/(home)/loading.tsx
@@ -0,0 +1,11 @@
+import styles from '@/styles/Home.module.css';
+
+export default function Loading() {
+  return (
+    <section className={styles.categoryBox}>
+      <div className={styles.title}>
+        <h2>Loading ... </h2>
+      </div>
+    </section>
+  );
+}
---
+++ src/app/list/[name]/loading.tsx
@@ -0,0 +1,11 @@
+import styles from '@/styles/Detail.module.css';
+
+export default function Loading() {
+  return (
+    <section className={styles.bookList}>
+      <div className={styles.title}>
+        <h2>Loading ... </h2>
+      </div>
+    </section>
+  );
+}
