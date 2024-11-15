6563fef4dc5132fe32040c65c22c95498152d17f
JAGORING <lovehaha11@gmail.com>
2024-11-16 02:44:58 +0900

    [Feat] Detail > fetch API 적용 및 내용 출력

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,7 +6,7 @@ export const metadata: Metadata = {
   title: 'Home',
 };
 
-export const API_URL = 'https://books-api.nomadcoders.workers.dev/lists';
+const API_URL = 'https://books-api.nomadcoders.workers.dev/lists';
 
 const fetchCategoryList = async () => {
   return fetch(API_URL)
@@ -18,11 +18,17 @@ const Home = async () => {
   const categoryList = await fetchCategoryList();
   return (
     <section className={styles.categoryBox}>
-      {categoryList.map((book: CategoryList) => (
-        <Link key={book.list_name_encoded} href={`/list/${book.list_name_encoded}`}>
-          <div className={styles.category}>{book.display_name}</div>
-        </Link>
-      ))}
+      <div className={styles.title}>
+        <h2>Explore Our Book Categories</h2>
+      </div>
+
+      <div className={styles.categoryList}>
+        {categoryList.map((book: CategoryList) => (
+          <Link key={book.list_name_encoded} href={`/list/${book.list_name_encoded}`}>
+            <div className={styles.category}>{book.display_name}</div>
+          </Link>
+        ))}
+      </div>
     </section>
   );
 };
---
+++ src/app/list/[name]/page.tsx
@@ -0,0 +1,45 @@
+import styles from '@/styles/Detail.module.css';
+import { Metadata } from 'next';
+import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: 'Detail',
+};
+
+const API_URL = 'https://books-api.nomadcoders.workers.dev/list';
+
+const fetchBookList = async (name: string) => {
+  return fetch(`${API_URL}?name=${name}`)
+    .then((res) => res.json())
+    .then((res) => res.results);
+};
+
+const Detail = async ({ params: { name } }: { params: { name: string } }) => {
+  const categoryList: Book = await fetchBookList(name);
+  return (
+    <section className={styles.bookList}>
+      <div className={styles.title}>
+        <h2>{categoryList.display_name}</h2>
+      </div>
+
+      <div className={styles.bookGrid}>
+        {categoryList.books.map((book) => (
+          <div className={styles.bookBox} key={book.title}>
+            <div className={styles.imageWrapper}>
+              <img src={book.book_image} alt={book.description} />
+            </div>
+            <div className={styles.bookInfo}>
+              <h3 className={styles.bookTitle}>{book.title}</h3>
+              <p className={styles.bookAuthor}>{book.author}</p>
+              <Link href={book.amazon_product_url || ''} target="_blank" className={styles.learnMoreButton}>
+                Buy Now
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default Detail;
