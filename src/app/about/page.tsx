42292da2a25771395204201cd5192091ae921a6a
JAGORING <lovehaha11@gmail.com>
2024-11-16 02:59:19 +0900

    [Feat] About > 페이지 생성 및 내용 추가

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,4 @@
+import styles from '@/styles/About.module.css';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -5,5 +6,17 @@ export const metadata: Metadata = {
 };
 
 export default function About() {
-  return <section>About</section>;
+  return (
+    <section className={styles.aboutBox}>
+      <div className={styles.imageWrapper}>
+        <img src="/about-us-img.jpg" alt="About Us" />
+      </div>
+
+      <div className={styles.title}>
+        <h2>About us</h2>
+      </div>
+      <p className={styles.content}>Welcome to the official explore for the New York Times Best Seller list explore.</p>
+      <p className={styles.content}>We hope you enjoy your stay!</p>
+    </section>
+  );
 }
